4c07df519be14cf7bc6f0a08d2b718daf65ba66e
Dor Wiser <dorw@wix.com>
2021-08-16 11:17:05 +0300

    init

--- src/Honeycomb/js/wix.galleries.honeycomb.js
+++ src/Honeycomb/js/wix.galleries.honeycomb.js
@@ -21,8 +21,8 @@ HoneycombController.prototype.updatePixelRatio = function(){
 		self.canvas.width *= devicePixelRatio;
 		self.canvas.height *= devicePixelRatio;
 
-		self.canvas.style.width = viewportSize.width + 'px';
-		self.canvas.style.height = viewportSize.height + 'px';
+		self.canvas.style.width = self.canvas.width + 'px';
+		self.canvas.style.height = self.canvas.height + 'px';
 
 		canvasContext.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0);
 	}
@@ -391,6 +391,7 @@ HoneycombController.prototype.updateCanvasSize = function(){
 	self.canvas.height = size.height
 	self.canvas.width = size.width
 	if (paper.view) paper.view.viewSize = [size.width, size.height]
+	self.updatePixelRatio();
 	Wix.setHeight(size.height);
 	return size
 }
