b34f69fa15439f534b0f50f5e6ed3ed0ed1cdd96
Dor Wiser <dorw@wix.com>
2021-08-16 11:13:59 +0300

    init

--- src/Honeycomb/js/wix.galleries.honeycomb.js
+++ src/Honeycomb/js/wix.galleries.honeycomb.js
@@ -391,7 +391,6 @@ HoneycombController.prototype.updateCanvasSize = function(){
 	self.canvas.height = size.height
 	self.canvas.width = size.width
 	if (paper.view) paper.view.viewSize = [size.width, size.height]
-	self.updatePixelRatio();
 	Wix.setHeight(size.height);
 	return size
 }
